868e6806ceab1b678c9f6733919ebe4cb959e57c
ohwoong2 <ohwoonge@gmail.com>
2024-09-30 16:28:02 +0900

    --

--- cicdbasic.sh
+++ cicdbasic.sh
@@ -2,27 +2,32 @@
 
 # 변수 설정
 JAR_FILE="SpringApp-0.0.1-SNAPSHOT.jar"
-DEPLOY_DIR="/home/username/step07cicd"
+DEPLOY_DIR="/var/jenkins_home/appjar"
+PORT=8999
+
+# 디렉토리 생성 (존재하지 않을 경우)
+mkdir -p $DEPLOY_DIR
 
 # 이전 JAR 파일 백업
 if [ -f "$DEPLOY_DIR/$JAR_FILE" ]; then
-  mv "$DEPLOY_DIR/$JAR_FILE" "$DEPLOY_DIR/$JAR_FILE.bak"
+    mv "$DEPLOY_DIR/$JAR_FILE" "$DEPLOY_DIR/${JAR_FILE}.bak"
 fi
 
 # 새로운 JAR 파일 복사
-# 이 스크립의 위치는 어디에 개발되어야 하나?
-cp $JAR_FILE $DEPLOY_DIR/$JAR_FILE
+# Jenkins 워크스페이스에서 JAR 파일 위치 가정
+cp "/var/jenkins_home/workspace/step02_jar_cicd/build/libs/$JAR_FILE" "$DEPLOY_DIR/$JAR_FILE"
 
 # Spring Boot 애플리케이션 재시작
-# 기존 8080 포트 사용 중인 프로세스 종료
-if  lsof -i :8999 > /dev/null; then
-  # 8080 포트가 사용 중일 경우 이전 프로세스를 종료
-   kill -9 $( lsof -t -i:8999)
+# 기존 포트 사용 중인 프로세스 종료
+PID=$(lsof -t -i:$PORT)
+if [ -n "$PID" ]; then
+    echo "Stopping existing application on port $PORT..."
+    kill $PID
+    sleep 5
 fi
 
 # 백그라운드에서 새로 실행
-# > $DEPLOY_DIR/app.log : 애플리케이션 로그를 app.log 파일에 저장하도록 구성
-# 정상 데이터 출력이 아닌 에러 메세지도 app.log에 출력 
-nohup java -jar $DEPLOY_DIR/$JAR_FILE > $DEPLOY_DIR/app.log 2>&1 &
+echo "Starting new application..."
+nohup java -jar "$DEPLOY_DIR/$JAR_FILE" > "$DEPLOY_DIR/app.log" 2>&1 &
 
 echo "배포완료 및 실행됩니다."
